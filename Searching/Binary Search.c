46dc377e387ca41a3035075c6a61f20d88519028
eccentriccoder01 <sagnikgraviton847@gmail.com>
2023-11-26 01:21:19 +0530

    Update Binary Search.c

--- Searching/Binary Search.c
+++ Searching/Binary Search.c
@@ -3,12 +3,12 @@
 int binary(int a[], int n, int data){
     int l=0,r=n;
     while(l<=r){
-        int m=l+(r-l)/2;
-        if(a[m]==data) return m;
-        if(a[m]<data) l=m+1;
-        else r=m-1;
+        int m=l+(r-l)/2; // Find middle index
+        if(a[m]==data) return m; // Element found
+        if(a[m]<data) l=m+1; // Search right half
+        else r=m-1; // Search left half
     }
-    printf("Element Doesn't Exist in The Array!"); return -1;
+    printf("Element Doesn't Exist in The Array!"); return -1; // Not found
 }
 int binaryRecursive(int a[], int l, int r, int data){
     // If array is not empty
