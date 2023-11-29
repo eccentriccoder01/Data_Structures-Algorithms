d1944240bbb1721fab26c38863609a6ca899a6ed
eccentriccoder01 <sagnikgraviton847@gmail.com>
2023-11-29 09:31:06 +0530

    Update Binary Search.c

--- Searching/Binary Search.c
+++ Searching/Binary Search.c
@@ -20,3 +20,17 @@ int binaryRecursive(int a[], int l, int r, int data){
     }
     printf("Element Doesn't Exist in The Array!"); return -1; // Element not found
 }
+int main(){
+    printf("Enter the Size of The Array: ");
+    int n; scanf("%d",&n);
+    int a[n];
+    printf("Enter the Elements: \n");
+    for(int i=0;i<n;i++) scanf("%d",&a[i]);
+    printf("Enter the Element to be Searched: ");
+    int data; scanf("%d",&data);
+    clock_t t = clock();
+    for(int j=0;j<100000;j++) binary(a,n,data);
+    t = clock() - t; 
+    double time_taken = ((double)t); // in seconds 
+    printf("Took %lf seconds to execute \n", time_taken); 
+}
