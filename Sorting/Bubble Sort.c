5fef57759fda036e3a6bd2ce781721ae5338506c
eccentriccoder01 <sagnikgraviton847@gmail.com>
2023-12-11 00:04:19 +0530

    Update Bubble Sort.c

--- Sorting/Bubble Sort.c
+++ Sorting/Bubble Sort.c
@@ -4,5 +4,11 @@ void swap(int *x, int *y){
     int t=*x; *x=*y; *y=t;
 }
 void bubble(int a[],int n){
-    for(int i=0;i<n-1;i++) for(int j=0;j<n;j++) if(a[i]>a[j]) swap(&a[i],&a[j]);
+    // Start bubble sort algorithm
+    for(int i=0;i<n-1;i++)
+        // Traverse array elements and compare
+        for(int j=0;j<n;j++)
+            if(a[i]>a[j])
+                // Swap if current element is greater than next
+                swap(&a[i],&a[j]);
 }
