74fa41249b53aa9faa5ccf1f4cb19f7ce50c1d63
eccentriccoder01 <sagnikgraviton847@gmail.com>
2023-12-24 10:51:13 +0530

    Update Static Linear Queue.c

--- Queues/Static Linear Queue.c
+++ Queues/Static Linear Queue.c
@@ -1,11 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
 struct Queue{int front,rear,size;unsigned capacity;int *a;};
+// create new queue
 struct Queue *newQueue(unsigned capacity){
+    // allocate memory for queue structure
     struct Queue *q=(struct Queue*)malloc(sizeof(struct Queue));
+
+    // initialize front and rear indices
     q->front==0;q->rear==capacity-1;
+
+    // set initial size and capacity
     q->size=0;q->capacity=capacity;
+
+    // allocate memory for queue array
     q->a=(int *)malloc(q->size*sizeof(int));
+
     return q;
 }
 int isFull(struct Queue *q){
