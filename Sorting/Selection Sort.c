4119332e84f98a24453677466c4069853bea7cdf
eccentriccoder01 <sagnikgraviton847@gmail.com>
2024-01-31 23:26:56 +0530

    Update Selection Sort.c

--- Sorting/Selection Sort.c
+++ Sorting/Selection Sort.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
-void swap(int *x,int *y){
-    int t=*x; *x=*y; *y=t;
+// Function to swap two integers
+void swap(int *, int *y) {    // Temporary variable to hold x's value
+   int t = *x;
+   
+   // Set x to y's value
+*x = *y;
+
+   // Set y to temporary variable's value
+   *y = t;
 }
 void selection(int a[],int n){
     for(int i=0;i<n-1;i++){
