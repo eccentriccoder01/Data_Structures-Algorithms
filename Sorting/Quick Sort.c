478d1c6c92f77c01c48da6051959903e527800c6
eccentriccoder01 <sagnikgraviton847@gmail.com>
2024-02-03 23:46:35 +0530

    Update Quick Sort.c

--- Sorting/Quick Sort.c
+++ Sorting/Quick Sort.c
@@ -10,14 +10,27 @@ void swap(int *, int *y) {    // Temporary variable to hold x's value
    // Set y to temporary variable's value
    *y = t;
 }
-int partition(int a[],int low,int high){
-    int pivot=a[high],i=low-1;
-    for(int j=low;j<high;j++){
-        if(a[j]<pivot){
-            i++; swap(&a[j],&a[i]);
-            j-=1
+int partition(int a[], low, int high) {
+    // choose pivot as last element
+    int pivot = a[high];
+    int i = low - 1;  // index for smaller elements
+
+    // traverse through all elements
+    for (int j = low; j < high; j++) {
+        // if current element is smaller than pivot
+        if (a[j] < pivot) {
+            // increment index of smaller element
+            i++;
+            // swap current element with element at i
+            swap(&a[j], &a[i]);
+            // move to next element
+            j -= 1;
         }
-    } swap(&a[i+1],&a[high]);
+    }
+    // swap pivot element with element at i+1
+    swap(&a[i+1], &a[high]);
+    // return index of pivot element
+    return i + 1;
 }
 void quickSort(int a[],int low,int high){
     int p=part(a,low,high);
