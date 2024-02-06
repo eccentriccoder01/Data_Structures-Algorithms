48f00aa2cb09cd2a55d629d6128a4d421a38bbb5
eccentriccoder01 <sagnikgraviton847@gmail.com>
2024-02-06 23:33:35 +0530

    Update Insertion Sort.c

--- Sorting/Insertion Sort.c
+++ Sorting/Insertion Sort.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
-void insertionSort(int a[],int n){
-    for(int i=1;i<n;i++){
-        int key=a[i],j=i-1;
-        while(j>=0 && a[j]>key){
-            a[j+1]=a[j]; j-=1;
-        } a[j+1]=key;
+void insertionSort( a[], int n) {
+    // Iterate through elements in the array, starting from the second element
+    for (int i = 1; i < n; i++) {
+ int key = a[i], j = i - 1;  // Store the current element and its previous index
+
+        // Move larger elements to the right of the current element
+        while (j >= 0 && a[j] > key) {
+            a[j + 1] = a[j];   // Shift element to the right
+            j -= 1;            // Decrease index
+        }
+
+        // Insert the key at its correct position
+        a[j + 1] = key;
     }
 }
