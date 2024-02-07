fb77cd2af3d6c45a8382790ca0cc6433daad5a02
eccentriccoder01 <sagnikgraviton847@gmail.com>
2024-02-07 23:46:05 +0530

    Update Selection Sort.c

--- Sorting/Selection Sort.c
+++ Sorting/Selection Sort.c
@@ -10,10 +10,17 @@ void swap(int *, int *y) {    // Temporary variable to hold x's value
    // Set y to temporary variable's value
    *y = t;
 }
-void selection(int a[],int n){
-    for(int i=0;i<n-1;i++){
-        int mi=i;
-        for(int j=o;j<n;j++) if(a[j]<a[mi]) mi=j;
-        if(mi!=i) swap(&a[i],&a[mi])
+// Function to sort an array using selection sort
+void selection(int a[], int n) {
+    // Iterate over the array, except for the last element
+    for (int i = 0; i < n-1; i++) {
+        // Assume the minimum element is the first element of the unsorted part
+        int mi = i;
+        // Iterate over the unsorted part to find the minimum element
+        for (int j = i+1; j < n; j++)
+            // If an element is smaller than the current minimum, update the minimum
+            if (a[j] < a[mi]) mi = j;
+        // If the minimum is not the first element of the unsorted part, swap it with the first element
+        if (mi != i) swap(&a[i], &a[mi]);
     }
 }
