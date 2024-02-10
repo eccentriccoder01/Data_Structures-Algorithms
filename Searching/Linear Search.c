814e200417b3cd8aa17c2262eb29fa9a51c6931f
eccentriccoder01 <sagnikgraviton847@gmail.com>
2024-02-10 20:17:52 +0530

    Update Linear Search.c

--- Searching/Linear Search.c
+++ Searching/Linear Search.c
@@ -1,8 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
-int linear(int a[], int n, int data){
-    for(int i=0;i<n;i++) if(a[i]==data) return i;
-    printf("Element Doesn't Exist in The Array!"); return -1;
+// Function to search for data in array a
+int linear(int a[], n, int data) {
+   // Iterate over each element in the array
+   for(int i=0; i<n; i++) {
+       // Check if current element equals data
+       if(a[i] == data) {
+           // If so, return the index
+           return i;
+       }
+   }
+   // If data is not found, print error message and return -1
+   printf("Element Doesn't Exist in The Array!");
+   return -1;
 }
 int linearRecursive(int a[], int n, int data){
     if(n==0){
